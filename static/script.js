5b88a3491d304739e61c618fd941f07c7eb991e3
Shivam Thapar <shivam.thapar1@gmail.com>
2013-04-06 13:06:06 -0700

    changed loading

--- static/script.js
+++ static/script.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 		$(".header2").hide();
 		$(".header-main").show();
 		$(".loading").show();
+		startEllipsis();
 	});
 	$(".assignment-type h4").click(function(){
 		var $icon= $(this).children(".collapse-icon");
@@ -16,3 +17,29 @@ $(document).ready(function(){
 		$collapse.collapse('toggle');
 	});    		
 })
+/*
+ * animated ellipsis
+ * 
+ * Created by Torsten Walter on 2008-04-06.
+ * Copyright playground.magicrising.de. Some rights reserved.
+ *
+ * This script may be copied and reproduced
+ * if this copyright notice is left intact.
+ */
+
+ellipsis = ['', '.', '..', '...'];
+var runEllipsis = false;
+function animateEllipsis(el, count) {
+	el.innerHTML = ellipsis[count%4];
+	if(runEllipsis == true) {
+	 window.setTimeout( function(){
+	   animateEllipsis(el, ++count);}, 250);
+	}
+}
+function startEllipsis() {
+	runEllipsis = true;
+	animateEllipsis(document.getElementById('ellipsisSpan'), 0);
+}
+function stopEllipsis() {
+	runEllipsis = false;
+}
