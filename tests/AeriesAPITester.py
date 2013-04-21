8534e72c98d18b421b070e1185d97863e4387328
Shivam Thapar <shivam.thapar1@gmail.com>
2013-04-21 11:06:11 -0700

    added hints on tester file

--- tests/AeriesAPITester.py
+++ tests/AeriesAPITester.py
@@ -1,6 +1,6 @@
 #Add tests for AeriesAPI here 
 import AeriesAPI
-s=AeriesAPI.AeriesAPI('','')
+s=AeriesAPI.AeriesAPI('user','pw')
 s.login()
 
 #test to check if periods are correctly fetched
@@ -8,6 +8,10 @@ s.login()
 #print "Classes: "
 #print s.getPeriods()
 
+#Test to check if correct period information is fetched
+#To test for a different class, put in a different class id.
+#To find class id, go to Aeries, right click on the class title (eg. AP-Biology) and copy the link address. Paste the link
+#address and look at the last little bit (&GrdBk=2100004). The number after &GrdBk is the classid (eg. 2100004)
 print "Spanish Assignments:"
-print s.getPeriodAssignments('3430307')
-#add new tests here
+print s.getPeriodAssignments('3430307')#change this classid
+#add new tests hereg
