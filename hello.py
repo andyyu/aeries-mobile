4d3e18ba2c97a7626f60d994aed590cd79e32103
Shivam Thapar <shivam.thapar1@gmail.com>
2013-04-21 22:27:54 -0700

    debug false

--- hello.py
+++ hello.py
@@ -77,4 +77,4 @@ def page_not_found(e):
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port, debug=True)
+	app.run(host='0.0.0.0', port=port, debug=False)
